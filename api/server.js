48006aa1f9246457a2c0e884c540fb6adc3583d9
JorPoon <jormpoon@gmail.com>
2020-08-17 16:44:38 -0400

    required socketio

--- api/server.js
+++ api/server.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const socketio = require("socket.io")
 const helmet = require("helmet");
 const cors = require("cors");
 
 const server = express();
 
 server.use(helmet());
-server.use(express.json());
 server.use(cors());
+server.use(express.json());
+
 
 
 server.get("/", (req, res ) => {
